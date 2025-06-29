435bcb6500d6f0c1a0992b11b75d5d1c2ad49e87
sjlee9908 <sjlee9908@naver.com>
2025-06-29 21:47:38 +0900

    tmp

--- feature/hub/src/main/java/com/anam145/wallet/feature/hub/domain/db/MiniAppDB.kt
+++ feature/hub/src/main/java/com/anam145/wallet/feature/hub/domain/db/MiniAppDB.kt
@@ -17,48 +17,4 @@ import kotlinx.coroutines.launch
 @Database(entities = [MiniApp::class], version = 1)
 abstract class MiniAppDB : RoomDatabase() {
     abstract fun miniAppDao(): MiniAppDao
-
-//
-//    companion object {
-//        @Volatile
-//        private var INSTANCE: MiniAppDB? = null
-//
-//        fun getDatabase(context: Context, scope: CoroutineScope): MiniAppDB { // CoroutineScope를 인자로 받도록 수정
-//            return INSTANCE ?: synchronized(this) {
-//                val instance = Room.databaseBuilder(
-//                    context.applicationContext,
-//                    MiniAppDB::class.java,
-//                    "mini_app"
-//                )
-//                    .fallbackToDestructiveMigration() // 앱 재시작때마다 reset
-//                    .addCallback(MiniAppDatabaseCallback(scope)) // ★★★ 콜백 추가 ★★★
-//                    .build()
-//                INSTANCE = instance
-//                instance
-//            }
-//        }
-//    }
-//
-//
-//    private class MiniAppDatabaseCallback(
-//        private val scope: CoroutineScope
-//    ) : RoomDatabase.Callback() {
-//        override fun onCreate(db: SupportSQLiteDatabase) { // 데이터베이스가 처음 생성될 때 호출됨
-//            super.onCreate(db)
-//            INSTANCE?.let { database ->
-//                scope.launch(Dispatchers.IO) {
-//                    val miniAppDao = database.miniAppDao()
-//                    miniAppDao.insertMiniApp(
-//                        MiniApp(
-//                            appId = "app_id_1",
-//                            name = "app_id_1_name",
-//                            type = MiniAppType.BLOCKCHAIN,
-//                            iconPath = "/path/to/app1_icon",
-//                            balance = "app_id_1_balance",
-//                        )
-//                    )
-//                }
-//            }
-//        }
-//    }
 }
