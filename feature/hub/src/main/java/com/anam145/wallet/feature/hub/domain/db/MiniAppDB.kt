60bdb3039ba334b3d1df447107b8468eed6a6535
sjlee9908 <sjlee9908@naver.com>
2025-07-01 18:59:58 +0900

    Update MiniAppDB.kt

--- feature/hub/src/main/java/com/anam145/wallet/feature/hub/domain/db/MiniAppDB.kt
+++ feature/hub/src/main/java/com/anam145/wallet/feature/hub/domain/db/MiniAppDB.kt
@@ -10,9 +10,6 @@ import androidx.sqlite.db.SupportSQLiteDatabase
 import com.anam145.wallet.core.common.model.MiniApp
 import com.anam145.wallet.core.common.model.MiniAppType
 import com.anam145.wallet.feature.hub.domain.dao.MiniAppDao
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 @Database(entities = [MiniApp::class], version = 1)
 abstract class MiniAppDB : RoomDatabase() {
